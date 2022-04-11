76e8302fb6d0568f8e79c256dacec16d23c91c87
Konstantin <iki0@yandex.ru>
2022-04-11 15:26:09 +0400

    Lesson2

---
+++ MetricsManager/MetricsManagerTests/MetricsManagerUnitTest.cs
@@ -0,0 +1,289 @@
+using System;
+using Xunit;
+using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MetricsManagerTests
+{
+    public class CpuMetricsControllerTests
+    {
+        private CpuMetricsController controller;
+
+        public CpuMetricsControllerTests()
+        {
+            controller = new CpuMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+    public class DotNetMetricsControllerTests
+    {
+        private DotNetMetricsController controller;
+
+        public DotNetMetricsControllerTests()
+        {
+            controller = new DotNetMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+    public class HddMetricsControllerTests
+    {
+        private HddMetricsController controller;
+
+        public HddMetricsControllerTests()
+        {
+            controller = new HddMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+    public class NetworkMetricsControllerTests
+    {
+        private NetworkMetricsController controller;
+
+        public NetworkMetricsControllerTests()
+        {
+            controller = new NetworkMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+    public class RamMetricsControllerTests
+    {
+        private RamMetricsController controller;
+
+        public RamMetricsControllerTests()
+        {
+            controller = new RamMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAgent(agentId, fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        [Fact]
+        public void GetMetricsByPercentileFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetricsByPercentileFromAllCluster(fromTime, toTime);
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
